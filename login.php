dad4787bea5a431f68daa53c723c996af7d65137
rabinacharya <42927286+rabinacharya@users.noreply.github.com>
2019-05-28 07:16:44 +1000

    Add files via upload

--- login.php
+++ login.php
@@ -1,99 +1,61 @@
+<?php
 
-<?php include('header.php')?>
-
-
-<div class="container" style="margin-top:20px">
-
-<section class="login" id="loginpg">
-  <div class="row">
-    <div class="container-fluid">
-      <div class="col-md-12">
-          <div class="login-page">
-            <div class="form ">
-              <h4 class="text-center">Staff Login</h4>
-               <?php echo form_open('login/index'); ?>
-
-            </div>
-        </div>
-      </div>
-    </div>
-    <hr>
-  </div>
-        <div class="row">
-          <div class="container-fluid">
-            <div class="col-md-12">
-              
-                   <span class="col-md-3"></span>
-                    
-                    
-                    
-                        <div class="form-group">
-                          <label for="email">Email address</label>
-                         <!-- <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">-->
-                          
-
-                        
-                          <?php echo form_input(['class'=>'form-control','placeholder'=>'Enter email','name'=>'uemail','value'=>set_value('uemail')]);?>
-
-                        </div>
-
-                       
-
-                    
-
-                       
-                       <?php echo form_error('uemail');  ?>
-                      
-                     
-                    
-                      
-                       
-                    
+/**
+ *  login
+ */
+class login extends CI_Controller
+{
+	
 
 
-                        <div class="form-group">
-                          <label for="password">Password</label>
-                          <!--<input type="password" class="form-control" id="password" placeholder="Password">
-                        </div>-->
-                             
-                               <?php echo form_password(['class'=>'form-control','placeholder'=>'Enter Password','name'=>'pass','value'=>set_value('pass')]);?>
+	
+	public function index()
+	{
 
-                          </div>
-                      
-                      
-                       
-                       <?php echo form_error('pass');  ?>
-                      
-                  
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('uemail','User Email','required|valid_email');
+		$this->form_validation->set_rules('pass','password','required|max_length[12]');
+		$this->form_validation->set_error_delimiters('<div class="text-danger">', '</div>');
+        
+     if ($this->form_validation->run()){
 
+      $uemail=$this-> input -> post('uemail');
+       $pass=$this-> input -> post('pass');
 
-                        <div class="buttons">
-                          <!--<button class="btn btn-login" type="submit">Staff - Login</button>-->
+      $this->load->model('loginmodel');
+      if($this->loginmodel->isvalidate($uemail,$pass))
 
-                          <?php echo form_submit(['type'=>'submit','class'=>'btn btn-login','value'=>'Staff Login']); ?>
+       {
+
+      	
+       $this->load->library('session');
+       $this->session ->set_userdata('email',$uemail);
+
+       $this->load->view('dashboard.php');
+
+      }
+
+      
+      else{
+
+      	echo "Details not matched";
+      }
+
+     }
+        else{
+
+        	$this->load->view('login');
+        }
+
+	}
 
 
 
 
 
-                        <button class="btn btn-default">
-                            <a href="<?php echo site_url("register");?>">Staff - Register</a>
-                          </button>
-                        </div>
-                      </form>
-
-                    </div>
-                    <span class="col-md-3"></span>
-
-                  </div>
-                </div>
-              
-            </div>
-
-</section>
-  
-</div>
 
 
+	
+}
 
-<?php include('footer.php')?>
+?>
