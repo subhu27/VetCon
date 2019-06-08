b0410d8ba28835ea10de16a6f6313e7bcd4bb965
subhu27 <45533510+subhu27@users.noreply.github.com>
2019-06-08 17:27:28 +1000

    display of users in controller

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -12,9 +12,45 @@ class Users extends CI_Controller {
 
 
 
-    public function users_info(){
+    public function index(){
+
+    	$result_rows['query']= $this->User_model->display_users();
+
+    		//var_dump($result_rows);
+
+    /*	foreach ($result_rows->result() as $row){
+    		$rows_count = $result_rows->num_rows();
+        	$fname = $row->vuser_fname;
+        	$lname = $row->vuser_lname;
+        	$name = $fname." ".$lname;
+        	$email = $row->vuser_email;
+        	$access = $row->vuser_access;
+        	$phone = $row->vuser_phone;
+        	$users_details=array('name'=>$name, 'email'=>$email, 'access'=>$access, 'phone'=>$phone);
+        	echo "<br> <br>";
+        	echo $name."<br>";
+        	echo $email."<br>";
+        	echo $access."<br>";
+        	echo $phone."<br>";
+        	
+    	}
+
+    	echo $rows_count;
+        //echo $row->body;
+    	//foreach ($return->result() as $value) {
+    	//	echo $value->vuser_fname;
+    	//}
+    	*/
+    	//var_dump($return);
+    	//$data['a']="hello word";
+    	$this->load->view('backend/backend_header_sidebar');
+    	$this->load->view('backend/users', $result_rows);
+    	$this->load->view('backend/backend_footer');
+    	//print_r($result);
+
     	
     }
+    	
 
 
 
