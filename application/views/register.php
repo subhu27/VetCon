0b53ea196c88f0bc0e59a9368a7953c6fa31b33d
rabinacharya <42927286+rabinacharya@users.noreply.github.com>
2019-06-08 19:31:48 +1000

    Add files via upload

--- application/views/register.php
+++ application/views/register.php
@@ -17,33 +17,36 @@
               <div class="row">
                     <span class="col-md-3"></span>
                     <div class="col-md-6">
-                      <form>
+                      <?php echo validation_errors(); ?>
+
+                      <?php echo form_open('register/register_user'); ?>
+                      <form method="post">
                         <div class="form-group name">
                           <label for="firstName">First Name</label>
-                          <input type="text" class="form-control" id="firstName" aria-describedby="firstNameHelp" placeholder="Enter First Name">
+                          <input type="text" name="firstName" class="form-control" id="firstName" aria-describedby="firstNameHelp" placeholder="Enter First Name" required>
                           <label for="lastName">Last Name</label>
-                          <input type="text" class="form-control" id="lastName" aria-describedby="lastNameHelp" placeholder="Enter Last Name">
+                          <input type="text" name="lastName" class="form-control" id="lastName" aria-describedby="lastNameHelp" placeholder="Enter Last Name" required>
                         </div>
                         <div class="form-group email">
                           <label for="email">Email address</label>
-                          <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+                          <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"  pattern="\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b"required>
                         </div>
                         <div class="form-group phoneNumber">
                           <label for="phoneNumber">Phone Number</label>
-                          <input type="tel" pattern=".{10}" class="form-control" id="phoneNumber" aria-describedby="phoneNumberHelp" placeholder="Enter Your Phone Number">
+                          <input type="tel" name="phoneNumber" pattern=".{10}" class="form-control" id="phoneNumber" aria-describedby="phoneNumberHelp" placeholder="Enter Your Phone Number" required>
                         </div>
                         <div class="form-group">
                           <label for="password">Password</label>
-                          <input type="password" class="form-control" id="password" placeholder="Password">
+                          <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
                         </div>
                         <div class="form-group">
                           <label for="rePassword">Re - Password</label>
-                          <input type="password" class="form-control" id="rePassword" placeholder="Retype Password">
+                          <input type="password" class="form-control" id="rePassword" placeholder="Retype Password" required>
                         </div>
                         <div class="buttons">
                           <button class="btn btn-login" type="submit">Staff - Register</button>
                           <button class="btn btn-login"> 
-                            <a href="<?php echo site_url("login");?>">Staff - Login</a>
+                            <a href="<?php echo site_url('login');?>">Staff - Login</a>
                           </button>
                         </div>
                       </form>
