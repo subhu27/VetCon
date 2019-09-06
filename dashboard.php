9f234089f5342e5c7b42555527e35ceb2d3ba7b9
rabinacharya <42927286+rabinacharya@users.noreply.github.com>
2019-09-06 14:14:30 +1000

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,30 @@
+
+     <div class="container-fluid">
+        <h1 class="mt-4">VetCon Stat</h1>
+
+        <div class="col-xs-6" >
+
+            <div class="row, text-left">
+        	
+        	  <a href="<?php echo ("Client") ?>"class="btn btn-success"> New Client </button> </a>
+       
+              <a href="" class="btn btn-success"> New Case  </a>
+
+              <a href="" class="btn btn-success"> New Disease  </a>
+       
+              <a href=""class="btn btn-success"> New Batch  </a>
+        
+              <a href="" class="btn btn-success"> New Farm  </a>
+      
+          </div>
+      </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+
+  </div>
+  <!-- /#wrapper -->
+
+  <!-- Bootstrap core JavaScript -->
+
+
+
