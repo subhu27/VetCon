a827e33ca73834a5960d1504129d00bba6945093
rabinacharya <42927286+rabinacharya@users.noreply.github.com>
2019-09-10 21:48:47 +1000

    Add files via upload

---
+++ application/views/backend/Cases.php
@@ -0,0 +1,80 @@
+<div class="container-fluid">
+
+        <div class="row"> 
+
+            <div class="col-4"> 
+
+                <h3 class="mt-4"> Cases   <button class="btn btn-success"> 
+              <a href="" > Add Case
+              </a> 
+            </button></td></h3>
+
+
+            </div>
+        </div>
+
+
+    
+
+    
+
+<div class="container">
+
+<table class="table table-hover">
+  <thead>
+    <tr>
+      
+      <th scope="col">Date</th>
+      <th scope="col">Disease Diagnosed</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      
+      <td></td>
+
+
+      <td> <button class="btn btn-success">
+              <a href="" > Edit 
+              </a> 
+            </button>  
+            <button class="btn btn-primary"> 
+              <a href="" >  View
+              </a>
+            </button></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      
+      <td></td>
+
+
+      <td><button class="btn btn-success">
+              <a href="" > Edit 
+              </a> 
+            </button>  
+            <button class="btn btn-primary"> 
+              <a href="" > View
+            </button></td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>  </td>
+
+
+      <td><button class="btn btn-success">
+              <a href="" > Edit 
+              </a> 
+            </button>  
+            <button class="btn btn-primary"> 
+              <a href="" > View
+              </a> 
+            </button></td>
+    </tr>
+  </tbody>
+</table>
+
+</div>
+  
