3955a6f7256f7ff97b603f8ebe1fe4d17e91b2fe
rabinacharya <42927286+rabinacharya@users.noreply.github.com>
2019-09-30 23:24:57 +1000

    client, batch and farm details view

---
+++ application/views/backend/Cfb_details.php
@@ -0,0 +1,142 @@
+<div class="container-fluid">
+
+        <div class="row"> 
+
+            <div class="col-4"> 
+
+                <h3 class="mt-4"> Farm/Batch  
+              <a href="" class="btn btn-success" > Add Batch
+              </a> 
+            </td></h3>
+
+
+            </div>
+        </div>
+
+
+    
+
+<!--Client Details-->    
+
+<div class="container">
+
+<table class="table table-hover">
+  <thead>
+  
+   <th scope="row" style="text-align:center">Client Details</th>
+
+  </thead>
+  <tbody>
+
+
+
+  
+
+    <tr>
+      <th scope="row">Client Name:</th>
+      
+      <td></td>
+
+
+    </tr>
+    <tr>
+      <th scope="row">Client Phone:</th>
+      
+      <td></td>
+
+
+     
+    </tr>
+    <tr>
+      <th scope="row">Client Address:</th>
+      <td>  </td>
+
+
+       
+    </tr>
+  </tbody>
+
+
+
+</table>
+
+</div>
+
+<!--Farm Details-->
+
+<div class="container">
+
+<table class="table table-hover">
+  <thead>
+  
+   <th scope="row" style="text-align:center">Farm Details</th>
+
+  </thead>
+  <tbody>
+
+    <tr>
+      <th scope="row">Farm Name:</th>
+      
+      <td></td>
+
+
+    </tr>
+    
+    <tr>
+      <th scope="row">Farm Address:</th>
+      <td>  </td>
+
+
+       
+    </tr>
+  </tbody>
+
+
+  
+</table>
+
+</div>
+
+<div class="container">
+
+<table class="table table-hover">
+  
+<thead>
+  
+  <th scope="row" style="text-align:center">Batch</th>
+  
+    <tr>
+      
+      <th scope="col">#</th>
+      <th scope="col">Start Date</th>
+      <th scope="col">End Date</th>
+      <th scope="col">Type</th>
+       <th scope="col">Total Count</th>
+        <th scope="col">Status</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    <tr>
+      <th scope="row">1</th>
+      
+      <td></td>
+
+
+    </tr>
+    
+    <tr>
+      <th scope="row">2</th>
+      <td>  </td>
+
+
+       
+    </tr>
+  </tbody>
+
+
+  
+</table>
+
+</div>
+  
